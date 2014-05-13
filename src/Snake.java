e8a32ec445ba59768694e579acaab2bcbc5205bd
alwaystest <ericceshi1@163.com>
2014-05-14 00:28:16 +0800

    添加注释

--- src/Snake.java
+++ src/Snake.java
@@ -1,15 +1,15 @@
-
+﻿
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 
 public class Snake {
 	
-	public LinkedList<Node> body;
+	public LinkedList<Node> body;//linkedlist相当于数据结构的链表，容易增加和删除。用这个存储蛇的身体位置
 	
 	public int dir=1;//dir 1=up 2=right 3=down 4=left
 
-	public Snake(){
+	public Snake(){//初始化为蛇增加身体，目前由6个Node组成。
 		body=new LinkedList<Node>();
 		body.add(new Node(100,100));
 		body.add(new Node(100,110));
@@ -20,7 +20,7 @@ public class Snake {
 		
 	}
 	
-	public void keyPressed(KeyEvent e) {
+	public void keyPressed(KeyEvent e) {//键盘监听，控制蛇的行进方向
 		int key = e.getKeyCode();
 		switch(key) {
 		case KeyEvent.VK_LEFT :
@@ -38,7 +38,7 @@ public class Snake {
 		}
 	}
 }
-class Node{
+class Node{//组成蛇的基础部分
 	public int x,y;
 	public Node(int x,int y){
 		this.x=x;
--- src/SnakeClient.java
+++ src/SnakeClient.java
@@ -1,24 +1,24 @@
-import java.awt.*;
+﻿import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 
-public class SnakeClient extends Frame {
+public class SnakeClient extends Frame {//主窗口
 	public static final int GAME_WIDTH = 800;
 	public static final int GAME_HEIGHT = 600;
 	
-	Snake mySnake = new Snake();
+	public Snake mySnake = new Snake();
 	
-	Image offScreenImage = null;
+	public Image offScreenImage = null;
 	
-	public void drawSnake(Graphics g) {
-		LinkedList<Node> body=mySnake.body;
+	public void drawSnake(Graphics g) {//包含蛇行进方向的判断，向前进方向加一个Node表示蛇头到此处，同时移除最后一个Node表示蛇尾离开此处
+		LinkedList<Node> body=mySnake.body;//将蛇身传递出来操作
 
-		Node head=(Node) body.getFirst();
+		Node head=(Node) body.getFirst();//以目前位置为基准，判断下一步Node的位置
 		
 		int x = head.x,
 			y = head.y;
 
-		switch(mySnake.dir){
+		switch(mySnake.dir){//等待提取步进值
 			case 1:
 				y-=5;
 				break;
@@ -36,8 +36,8 @@ public class SnakeClient extends Frame {
 		body.addFirst(nnode);
 		body.removeLast();
 
-		ListIterator list=body.listIterator(0);
-		while(list.hasNext()){
+		ListIterator list=body.listIterator(0);//ListIterator是对ListLink的迭代器，详见api文档的Iterator，类似数组的指针，这个可以后移next()，也可以前移previous()，直接用Iterator也可以，但不能前移
+		while(list.hasNext()){//将蛇身画完为止
 			
 			Node paint=(Node) list.next();
 			drawNode(g,paint);
@@ -47,10 +47,10 @@ public class SnakeClient extends Frame {
 	public void drawNode(Graphics gg,Node n){
 		
 		gg.setColor(Color.RED);
-		gg.fillRect(n.x,n.y,10,10);//waiting for bring number out
+		gg.fillRect(n.x,n.y,10,10);//等待提取宽度
 	}
 	
-	public void update(Graphics g) {
+	public void update(Graphics g) {//画图，双缓冲，防止闪烁
 		if(offScreenImage == null) {
 			offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
 		}
@@ -63,7 +63,7 @@ public class SnakeClient extends Frame {
 		g.drawImage(offScreenImage, 0, 0, null);
 	}
 
-	public void lauchFrame() {
+	public void lauchFrame() {//对窗口的一系列设置
 		this.setLocation(40, 30);
 		this.setSize(GAME_WIDTH, GAME_HEIGHT);
 		this.setTitle("Snaker");
@@ -101,7 +101,7 @@ public class SnakeClient extends Frame {
 		}
 	}
 	
-	private class KeyMonitor extends KeyAdapter {
+	private class KeyMonitor extends KeyAdapter {//将对键盘的操作传到Snake的监听器中
 
 		public void keyPressed(KeyEvent e) {
 			mySnake.keyPressed(e);
