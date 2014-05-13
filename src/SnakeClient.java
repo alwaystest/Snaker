2a5d0957432746b8d17729f08ae7ea1d15075641
alwaystest <ericceshi1@163.com>
2014-05-13 19:28:14 +0800

    0-2
    标题 不可调整大小 关闭窗口功能

--- src/SnakeClient.java
+++ src/SnakeClient.java
@@ -1,10 +1,18 @@
-import java.awt.Frame;
+import java.awt.*;
+import java.awt.event.*;
 
 public class SnakeClient extends Frame {
 	
 	public void lauchFrame() {
 		this.setLocation(400, 300);
 		this.setSize(800, 600);
+		this.setTitle("Snaker");
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		this.setResizable(false);
 		setVisible(true);
 	}
 
