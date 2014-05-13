32fa54d9b03936ef421a9e4866353878f8ab7204
alwaystest <ericceshi1@163.com>
2014-05-13 19:30:54 +0800

    0-4
    提取变量 移动

--- src/SnakeClient.java
+++ src/SnakeClient.java
@@ -3,11 +3,15 @@ import java.awt.event.*;
 
 public class SnakeClient extends Frame {
 	
+	int x = 50, y = 50;
+	
 	public void paint(Graphics g) {
 		Color c = g.getColor();
 		g.setColor(Color.RED);
-		g.fillRect(50, 50, 30, 30);
+		g.fillRect(x, y, 30, 30);
 		g.setColor(c);
+		
+		y += 5;
 	}
 
 	public void lauchFrame() {
@@ -22,11 +26,28 @@ public class SnakeClient extends Frame {
 		this.setResizable(false);
 		this.setBackground(Color.GREEN);
 		setVisible(true);
+		
+		new Thread(new PaintThread()).start();
 	}
 
 	public static void main(String[] args) {
 		SnakeClient sc = new SnakeClient();
 		sc.lauchFrame();
+	}
+	
+	private class PaintThread implements Runnable {
+
+		public void run() {
+			while(true) {
+				repaint();
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
 	}
 
 }
