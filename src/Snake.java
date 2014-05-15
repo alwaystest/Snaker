bdd2bfaab759143889e35ce545f96218da6ce11e
alwaystest <ericceshi1@163.com>
2014-05-16 01:53:11 +0800

    模拟蛇

--- src/Snake.java
+++ src/Snake.java
@@ -8,6 +8,9 @@ public class Snake {
 	public LinkedList<Node> body;//linkedlist相当于数据结构的链表，容易增加和删除。用这个存储蛇的身体位置
 	
 	public int dir=1;//dir 1=up 2=right 3=down 4=left
+	public int olddir=1;
+	public int turn;
+	public int size;//链表获取size需要遍历，提取出来方便操作
 
 	public Snake(){//初始化为蛇增加身体，目前由6个Node组成。
 		body=new LinkedList<Node>();
@@ -17,14 +20,19 @@ public class Snake {
 		body.add(new Node(100,130));
 		body.add(new Node(100,140));
 		body.add(new Node(100,150));
-	//	body.add(new Node(100,160));
+		body.add(new Node(100,160));
+		body.add(new Node(100,170));
+		body.add(new Node(100,180));
+		body.add(new Node(100,190));
+		body.add(new Node(100,200));
+		body.add(new Node(100,210));
 		size=body.size();
 	}
 	
-	public int size;//链表获取size需要遍历，提取出来方便操作
-	
 	public void keyPressed(KeyEvent e) {//键盘监听，控制蛇的行进方向
+		olddir=dir;
 		int key = e.getKeyCode();
+		
 		switch(key) {
 		case KeyEvent.VK_LEFT :
 			dir=4;
@@ -39,6 +47,7 @@ public class Snake {
 			dir=3;
 			break;
 		}
+		turn=1;
 	}
 }
 class Node{//组成蛇的基础部分
