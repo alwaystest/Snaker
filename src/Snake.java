2794568974e42a85aad8ab1ea75516f153a6b2fd
alwaystest <ericceshi1@163.com>
2014-05-16 02:26:27 +0800

    Merge branch '雏形' of https://github.com/alwaystest/Snaker into 雏形
    Conflicts:
    	README.md
    	src/Snake.java
    	src/SnakeClient.java

--- src/Snake.java
+++ src/Snake.java
@@ -1,37 +1,56 @@
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class Snake {
-	int x, y;
+	
+	public LinkedList<Node> body;
+	
+	public int dir=1;//dir 1=up 2=right 3=down 4=left
+	public int size;//链表获取size需要遍历，提取出来方便操作
 
-	public Snake(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public Snake(){
+		body=new LinkedList<Node>();
+		body.add(new Node(100,100));
+		body.add(new Node(100,110));
+		body.add(new Node(100,120));
+		body.add(new Node(100,130));
+		body.add(new Node(100,140));
+		body.add(new Node(100,150));
+		body.add(new Node(100,160));
+		body.add(new Node(100,170));
+		body.add(new Node(100,180));
+		body.add(new Node(100,190));
+		body.add(new Node(100,200));
+		body.add(new Node(100,210));
+		size=body.size();
 	}
 	
-	public void draw(Graphics g) {
-		Color c = g.getColor();
-		g.setColor(Color.RED);
-		g.fillRect(x, y,30, 30);
-		g.setColor(c);
-	}
-	
-	public void keyPressed(KeyEvent e) {
+	public void keyPressed(KeyEvent e) {//键盘监听，控制蛇的行进方向
 		int key = e.getKeyCode();
+		
 		switch(key) {
 		case KeyEvent.VK_LEFT :
-			x -= 5;
+			dir=4;
 			break;
 		case KeyEvent.VK_UP :
-			y -= 5;
+			dir=1;
 			break;
 		case KeyEvent.VK_RIGHT :
-			x += 5;
+			dir=2;
 			break;
 		case KeyEvent.VK_DOWN :
-			y += 5;
+			dir=3;
 			break;
 		}
-	}
 }
+class Node{//组成蛇的基础部分
+	public int x,y;
+	public Node(int x,int y){
+		this.x=x;
+		this.y=y;
+	}
+	
+	
+		}
--- src/SnakeClient.java
+++ src/SnakeClient.java
@@ -1,19 +1,105 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
-public class SnakeClient extends Frame {
+public class SnakeClient extends Frame {//主窗口
 	public static final int GAME_WIDTH = 800;
 	public static final int GAME_HEIGHT = 600;
-	
-	Snake mySnake = new Snake(50, 50);
+	public static int NodeWidth=10;//步进 node宽度
+	public static int[][] check=new int[GAME_WIDTH][GAME_HEIGHT];
+	public static boolean runstate=true;
+	public Snake mySnake = new Snake();
 	
 	Image offScreenImage = null;
 	
-	public void paint(Graphics g) {
-		mySnake.draw(g);
+	public void drawSnake(Graphics g) {//包含蛇行进方向的判断，向前进方向加一个Node表示蛇头到此处，同时移除最后一个Node表示蛇尾离开此处
+		LinkedList<Node> body=mySnake.body;//将蛇身传递出来操作
+		if(mySnake.size%2==0){
+		if(z==0)//配合drawNode的变形
+			z++;
+		else
+			z--;
+		}
+		cturn=mySnake.turn;
+		Node head=(Node) body.getFirst();//以目前位置为基准，判断下一步Node的位置
+		Node end=(Node) body.getLast();
+		int x = head.x,
+			y = head.y,
+			x1=end.x,
+			y1=end.y;
+		switch(mySnake.dir){//已提取步进值   
+			case 1:
+				y-=NodeWidth;
+				/*if(z==0){
+					x-=1;
+					z=1;
+				}
+				else{
+					x+=1;
+					z=0;
+				}*/
+				break;
+			case 2:
+				x+=NodeWidth;
+				/*if(z==0){
+					y-=1;
+					z=1;
+				}
+				else{
+					y+=1;
+					z=0;
+				}*/
+				break;
+			case 3:
+				y+=NodeWidth;
+				/*if(z==0){
+					x-=1;
+					z=1;
+				}
+				else{
+					x+=1;
+					z=0;
+				}*/
+				break;
+			case 4:
+				x-=NodeWidth;
+				/*if(z==0){
+					y-=1;
+					z=1;
+				}
+				else{
+					y+=1;
+					z=0;
+				}*/
+				break;
+		}
+		if (x<=0||x>=GAME_WIDTH||y<=0||y>=GAME_HEIGHT){
+			Thread.yield();
+		}
+		else{
+			if(check[x][y]==1)
+				System.out.println("Failed");
+			Node nnode=new Node(x,y);
+			body.addFirst(nnode);
+			check[x][y]=1;
+			body.removeLast();
+			check[x1][y1]=0;
+		}
+
+		ListIterator list=body.listIterator(0);//ListIterator是对ListLink的迭代器，详见api文档的Iterator，类似数组的指针，这个可以后移next()，也可以前移previous()，直接用Iterator也可以，但不能前移
+		while(list.hasNext()){//将蛇身画完为止 不进行强制转换会报错
+			Node paint=(Node) list.next();
+			drawNode(g,paint);
+		}
 	}
+
+	public void drawNode(Graphics gg,Node n){
+		gg.setColor(Color.RED);
+				gg.fillRect(n.x,n.y,NodeWidth,NodeWidth);
+		}
 	
-	public void update(Graphics g) {
+	
+	public void update(Graphics g) {//画图，双缓冲，防止闪烁
 		if(offScreenImage == null) {
 			offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
 		}
@@ -22,12 +108,12 @@ public class SnakeClient extends Frame {
 		gOffScreen.setColor(Color.GREEN);
 		gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 		gOffScreen.setColor(c);
-		paint(gOffScreen);
+		drawSnake(gOffScreen);
 		g.drawImage(offScreenImage, 0, 0, null);
 	}
 
-	public void lauchFrame() {
-		this.setLocation(400, 300);
+	public void lauchFrame() {//对窗口的一系列设置
+		this.setLocation(40, 30);
 		this.setSize(GAME_WIDTH, GAME_HEIGHT);
 		this.setTitle("Snaker");
 		this.addWindowListener(new WindowAdapter() {
@@ -53,10 +139,10 @@ public class SnakeClient extends Frame {
 	private class PaintThread implements Runnable {
 
 		public void run() {
-			while(true) {
+			while(runstate) {
 				repaint();
 				try {
-					Thread.sleep(50);
+					Thread.sleep(200);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -64,9 +150,12 @@ public class SnakeClient extends Frame {
 		}
 	}
 	
-	private class KeyMonitor extends KeyAdapter {
+	private class KeyMonitor extends KeyAdapter {//将对键盘的操作传到Snake的监听器中
 
 		public void keyPressed(KeyEvent e) {
+			int key = e.getKeyCode();
+			if(key==KeyEvent.VK_P)
+				runstate=!runstate;
 			mySnake.keyPressed(e);
 		}
 		
